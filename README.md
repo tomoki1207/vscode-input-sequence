6a1b2dada830891ff4dcd5e61339df2a0da5e497
tomoki1207 <tomoki1207@gmail.com>
2016-09-27 18:45:21 +0900

    create extension.

--- README.md
+++ README.md
@@ -1,65 +1,27 @@
-# vscode-input-sequence README
+# vscode-input-sequence
 
-This is the README for your extension "vscode-input-sequence". After writing up a brief description, we recommend including the following sections.
+[sequential-number](https://atom.io/packages/sequential-number) for VSCode.
 
-## Features
+## Usage
 
-Describe specific features of your extension including screenshots of your extension in action. Image paths are relative to this README file.
+### Default Keymap
+<kbd>ctrl</kbd> + <kbd>alt</kbd> + <kbd>0</kbd> => Open the input panel !
 
-For example if there is an image subfolder under your extension project workspace:
+### Syntax Rules
 
-\!\[feature X\]\(images/feature-x.png\)
+`<start> <operator> <step> : <digit> : <radix>`
 
-> Tip: Many popular extensions utilize animations. This is an excellent way to show off your extension! We recommend short, focused animations that are easy to follow.
+| Key                                   | Default | Definition                                                                                                                                      |
+| :------------------------------------ | :------ | :---------------------------------------------------------------------------------------------------------------------------------------------- |
+| **start**                             | `""`    | It specifies the number that you start typing an integer.                                                                                       |
+| **operator** <small>(optinal)</small> | `+`     | It specifies the generation rules of consecutive numbers in the `+` or `-`. The sign of the increment(`++`) and decrement(`--`) also available. |
+| **step** <small>(optinal)</small>     | `1`     | It specifies the integer to be added or subtracted.                                                                                             |
+| **digit** <small>(optinal)</small>    | `0`     | It specifies of the number of digits in the integer.                                                                                            |
+| **radix** <small>(optinal)</small>    | `10`    | It specifies an integer between 2 and 36 that represents radix.                                                                                 |
 
-## Requirements
+### More infomation and Examples
 
-If you have any requirements or dependencies, add a section describing those and how to install and configure them.
+See [sequential-number](https://atom.io/packages/sequential-number).
 
-## Extension Settings
-
-Include if your extension adds any VS Code settings through the `contributes.configuration` extension point.
-
-For example:
-
-This extension contributes the following settings:
-
-* `myExtension.enable`: enable/disable this extension
-* `myExtension.thing`: set to `blah` to do something
-
-## Known Issues
-
-Calling out known issues can help limit users opening duplicate issues against your extension.
-
-## Release Notes
-
-Users appreciate release notes as you update your extension.
-
-### 1.0.0
-
-Initial release of ...
-
-### 1.0.1
-
-Fixed issue #.
-
-### 1.1.0
-
-Added features X, Y, and Z.
-
------------------------------------------------------------------------------------------------------------
-
-## Working with Markdown
-
-**Note:** You can author your README using Visual Studio Code.  Here are some useful editor keyboard shortcuts:
-
-* Split the editor (`Cmd+\` on OSX or `Ctrl+\` on Windows and Linux)
-* Toggle preview (`Shift+CMD+V` on OSX or `Shift+Ctrl+V` on Windows and Linux)
-* Press `Ctrl+Space` (Windows, Linux) or `Cmd+Space` (OSX) to see a list of Markdown snippets
-
-### For more information
-
-* [Visual Studio Code's Markdown Support](http://code.visualstudio.com/docs/languages/markdown)
-* [Markdown Syntax Reference](https://help.github.com/articles/markdown-basics/)
-
-**Enjoy!**
+## LICENSE
+[LICENSE](./LICENSE)
--- extension.js
+++ extension.js
@@ -1,26 +1,46 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
+'use strict';
 var vscode = require('vscode');
+var Range = vscode.Range;
+var Position = vscode.Position;
+var syntaxRegex = /^([+\-]?[\da-fA-F]+(?:\.\d+)?)\s*([+\-]|(?:\+\+|\-\-))?\s*(\d+)?\s*(?:\:\s*(\d+))?\s*(?:\:\s*(\d+))?$/;
 
 // this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 function activate(context) {
+  var insertCmd = vscode.commands.registerCommand('insertSequentialNumbers', function () {
+    var editor = vscode.window.activeTextEditor;
+    var initialSelections = editor.selections.sort(sortSelection);
+    var inputOptions = {};
+    var undoStopBefore = true;
+    inputOptions.placeHolder = "<start> <operator> <step> : <digit> : <radix>";
+    inputOptions.validateInput = function (param) {
+      if (param === "") {
+        perform(initialSelections, editor, {}, { undoStopBefore: undoStopBefore, undoStopAfter: false });
+        return;
+      }
+      var test = parseInput(param);
+      if (!test) {
+        return 'Syntax error. The rule is "<start> <operator> <step> : <digit> : <radix>".';
+      }
+      // realtime simulate
+      perform(initialSelections, editor, test, { undoStopBefore: undoStopBefore, undoStopAfter: false });
+      undoStopBefore &= false;
+    };
+    vscode.window.showInputBox(inputOptions)
+      .then(function (value) {
+        if (!value && value !== 0) {
+          // undo
+          if (!undoStopBefore) {
+            vscode.commands.executeCommand("undo");
+          }
+          return;
+        }
+        // confirm input.
+        var options = parseInput(value);
+        perform(initialSelections, editor, options, { undoStopBefore: false, undoStopAfter: true });
+      });
+  });
 
-    // Use the console to output diagnostic information (console.log) and errors (console.error)
-    // This line of code will only be executed once when your extension is activated
-    console.log('Congratulations, your extension "vscode-input-sequence" is now active!');
-
-    // The command has been defined in the package.json file
-    // Now provide the implementation of the command with  registerCommand
-    // The commandId parameter must match the command field in package.json
-    var disposable = vscode.commands.registerCommand('extension.sayHello', function () {
-        // The code you place here will be executed every time your command is executed
-
-        // Display a message box to the user
-        vscode.window.showInformationMessage('Hello World!');
-    });
-
-    context.subscriptions.push(disposable);
+  context.subscriptions.push(insertCmd);
 }
 exports.activate = activate;
 
@@ -28,3 +48,91 @@ exports.activate = activate;
 function deactivate() {
 }
 exports.deactivate = deactivate;
+
+function sortSelection(a, b) {
+  return a.anchor.line - b.anchor.line || a.anchor.character - b.anchor.character;
+}
+
+function perform(initial, editor, options, undoStop) {
+  var currentSelection = editor.selections.sort(sortSelection);
+  editor.edit(function (builder) {
+    initial.forEach(function (selection, index) {
+      var endOfInitialSelection = selection.end.character + (currentSelection[index].start.character - selection.start.character);
+      builder.replace(new Range(new Position(selection.end.line, endOfInitialSelection), currentSelection[index].end), calculate(index, options));
+    });
+  }, undoStop);
+}
+
+function parseInput(input) {
+  var matches = input.match(syntaxRegex);
+  if (!matches) {
+    return null;
+  }
+
+  var radix = matches[5] ? parseInt(matches[5], 10) : 10;
+  var start = parseInt(matches[1], radix);
+  var operator = matches[2] || "+";
+  var step = isNaN(matches[3]) ? 1 : parseInt(matches[3], 10);
+
+  var digit = parseInt(matches[4], 10);
+  if (isNaN(digit)) {
+    digit = (start.toString() === matches[1]) ? 0 : matches[1].length;
+    if (/^[+\-]/.test(matches[1])) {
+      digit = Math.max(digit - 1, 0);
+    }
+  }
+  return {
+    start: start,
+    digit: digit,
+    operator: operator,
+    step: step,
+    radix: radix,
+    input: input
+  };
+}
+
+function calculate(index, options) {
+  var value = NaN;
+  switch (options.operator) {
+    case "++":
+      value = options.start + index;
+      break;
+    case "--":
+      value = options.start - index;
+      break;
+    case "+":
+      value = options.start + (index * options.step);
+      break;
+    case "-":
+      value = options.start - (index * options.step);
+      break;
+    default:
+      return "";
+  }
+
+  value = paddingZero(value, options.digit, options.radix);
+  var hasAlpha = options.input.match(/([a-fA-F])/);
+  if (hasAlpha) {
+    // for hex.
+    return hasAlpha[1] == hasAlpha[1].toLowerCase() ? value.toLowerCase() : value.toUpperCase();
+  }
+  return value;
+}
+
+function paddingZero(num, dig, radix) {
+  if (!dig) {
+    dig = 0;
+  }
+  if (!radix) {
+    radix = 10;
+  }
+  var number = num.toString(radix);
+  var numAbs = number.replace("-", "");
+  var digit = Math.max(numAbs.length, dig);
+  var result = "";
+  if (0 <= number.indexOf("-")) {
+    result += "-";
+  }
+  result += (Array(digit).join("0") + numAbs).slice(digit * -1);
+  return result;
+}
